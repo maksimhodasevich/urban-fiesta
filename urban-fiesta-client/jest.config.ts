8b5719c15235e037e7b8c450910c2576a31090ed
maksimhodasevich <maksim.hodasevich@gmail.com>
2021-03-18 23:45:48 +0300

    fix structure a bit

---
+++ urban-fiesta-client/jest.config.ts
@@ -0,0 +1,15 @@
+//TODO: make tests work with jest and get index.ts out of test coverage
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "json",
+  ],
+}
